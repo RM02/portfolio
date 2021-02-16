6a32787f4791919ce04c4cf7faa050f3949ad59b
RM02 <rafamark244@gmail.com>
2021-02-15 23:22:32 -0400

    initial commit

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+  	HomeComponent,
+  	HeaderComponent,
+  	FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
